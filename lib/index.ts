64e646e1ee98eeaed27dd5f34b2b04a685974197
Tine Jozelj <tine.jozelj@tjo.space>
2018-02-20 15:47:04 +0100

    build: fixed build process

---
+++ lib/index.ts
@@ -0,0 +1,6 @@
+export * from './raven.constants';
+export * from './raven.interceptor.abstract';
+export * from './raven.interceptor.mixin';
+export * from './raven.interfaces';
+export * from './raven.module';
+export * from './raven.providers';
