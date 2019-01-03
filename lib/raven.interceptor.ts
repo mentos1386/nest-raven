fa23a76fddd0c10290d6e40db122ddaf30023d44
Tine Jozelj <tine@tjo.space>
2019-01-03 11:50:45 +0100

    test: implement all the tests to work with @sentry/minimal

--- lib/raven.interceptor.ts
+++ lib/raven.interceptor.ts
@@ -6,6 +6,7 @@ import { IRavenInterceptorOptions } from './raven.interfaces';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import * as Sentry from '@sentry/minimal'
+import { Scope } from '@sentry/hub';
 
 @Injectable()
 export class RavenInterceptor implements NestInterceptor {
@@ -26,20 +27,25 @@ export class RavenInterceptor implements NestInterceptor {
     return call$.pipe(
       tap(null, (exception) => {
         if (this.shouldReport(exception)) {
-          Sentry.withScope(scope => {
-            scope.setExtra('req', httpRequest);
-            if (userData) scope.setUser(userData)
-            if (this.options.level) scope.setLevel(this.options.level);
-            if (this.options.fingerprint) scope.setFingerprint(this.options.fingerprint);
-            for (const tag in this.options.tags) {
-              scope.setTag(tag, this.options.tags[tag])
-            }
-
-            Sentry.captureException(exception);
-          });
+          Sentry.withScope(scope => this.captureException(scope, httpRequest, userData, exception));
         }
       })
     );
+  }
+
+  private captureException(scope: Scope, httpRequest, userData, exception): void {
+    scope.setExtra('req', httpRequest);
+    if (userData) scope.setUser(userData)
+    if (this.options.level) scope.setLevel(this.options.level);
+    if (this.options.fingerprint) scope.setFingerprint(this.options.fingerprint);
+    if (this.options.extra) for (const key in this.options.extra) {
+      if(this.options.extra.hasOwnProperty(key)) scope.setExtra(key, this.options.extra[key]);
+    }
+    for (const tag in this.options.tags) {
+      scope.setTag(tag, this.options.tags[tag])
+    }
+
+    Sentry.captureException(exception);
   }
   
   private shouldReport(exception: any): boolean {
