95994af7e0eaaef04fe5b3617adb779fce810c49
Tine Jozelj <tine.jozelj@tjo.space>
2019-05-22 22:07:50 +0200

    test: add test-project to test everything on sentry
    not autmated test, just ment to be used by developers to test everything with sentry

---
+++ test/test-project/src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { AppService } from './app.service';
+import { RavenInterceptor } from '../../../lib';
+
+@Controller()
+@UseInterceptors(new RavenInterceptor())
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
---
+++ test/test-project/src/app.service.ts
@@ -0,0 +1,9 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    throw new InternalServerErrorException('This is not good?');
+    return 'Hello World!';
+  }
+}
